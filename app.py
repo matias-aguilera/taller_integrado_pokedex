0f4e79963c7c5c795c189cfe033f9bdbabe9f00c
HexDrifter <42625632+HexDrifter@users.noreply.github.com>
2024-06-25 00:00:14 -0400

    Uploaded templates, index, items, pokedex and pokemon pages

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+
+def index():
+    #with open("data/pokemondb.json", "r", encoding="utf-8") as f:
+    #    datos = json.load(f)
+    return render_template("index.html")
+
+@app.route("/pokedex/")
+def pokedex():
+    return render_template("pokedex.html")
+
+@app.route("/items/")
+def items():
+    return render_template("items.html")
+
+@app.route("/poke/")
+def pokemon():
+    return render_template("pokemon.html")
+
+#@app.route("/p/<string:slug>/")
+#def show_post(slug):
+#    return render_template("post_view.html", slug_title=slug)
+    
+#@app.route("/admin/post/")
+#@app.route("/admin/post/<int:post_id>")
+#def post_form(post_id=None):
+#    return render_template("admin/post_form.html", post_id=post_id)
