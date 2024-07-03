413984931b3853630d12780da565fe6c79d29ff5
HexDrifter <42625632+HexDrifter@users.noreply.github.com>
2024-07-03 02:46:37 -0400

    Created Items menu, item, updated pokedex and pokemon view, updated database, fixed some bugs

--- app.py
+++ app.py
@@ -3,41 +3,50 @@ import json
 
 app = Flask(__name__)
 
-def loaddb():
-    with open("data/pokemondb.json", "r", encoding="utf-8") as f:
-        pokedb = json.load(f)
-    return pokedb["datos"]
+def loaddb(dbroute):
+    with open(dbroute, "r", encoding="utf-8") as f:
+        db = json.load(f)
+    return db["datos"]
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/prev/<int:dex_id>")
-def poke_prev(dex_id):
-    pokedexdb = loaddb()
+@app.route("/pokemon/<int:dex_id>")
+def pokemon(dex_id):
+    pokedexdb = loaddb("data/pokemondb.json")
     pokequery = next((item for item in pokedexdb if item["id"] == dex_id), None)
     if pokequery is None:
         abort(404)
-    return render_template("poke_prev.html", poke = pokequery)
+    return render_template("pokemon.html", poke = pokequery)
 
 @app.route("/pokedex/")
 def pokedex():
-    pokedexdb = loaddb()
-    return render_template("pokedex.html", dex=pokedexdb, n = 151)
+    pokedexdb = loaddb("data/pokemondb.json")
+    return render_template("pokedex.html", dex=pokedexdb, n = 386)
 
 @app.route("/items/")
 def items():
-    return render_template("items.html")
+    itemdb = loaddb("data/itemdb.json")
+    return render_template("items.html", items=itemdb)
 
-@app.route("/poke/")
-def pokemon():
-    return render_template("pokemon.html")
+@app.route("/item/<itemname>")
+def item(itemname):
+    itemdb = loaddb("data/itemdb.json")
+    itemquery = next(i for i in itemdb if i["name"]== itemname)
+    if itemquery is None:
+        abort(404)
+    return render_template("item.html", iteminfo = itemquery)
+
 
 @app.route("/bayas/")
 def bayas():
     return render_template("bayas.html")    
 
+@app.route("/extras/")
+def extras():
+    return render_template("extras.html")
 #@app.route("/p/<string:slug>/")
 #def show_post(slug):
 #    return render_template("post_view.html", slug_title=slug)
